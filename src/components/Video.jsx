88a6fb357f48c760e19df477661abcb444a78f9e
masum-abrar <masum.abrar02@gmail.com>
2024-04-30 22:36:48 +0600

    functionality updated

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -6,7 +6,7 @@ export const Video = () => {
             <h2>Video Section</h2>
             <video controls>
                 <source src={video} type="video/mp4" />
-                Your browser does not support the video tag.
+               
             </video>
             <p>This is a video section demonstrating some content.</p>
         </div>
