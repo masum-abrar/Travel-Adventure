c062720dc0e8c3d0c001b043b09f597e9fea0390
masum-abrar <masum.abrar02@gmail.com>
2024-04-30 22:50:20 +0600

    design updated

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,11 +1,11 @@
 // import video from '../assets/vid.mp4';
-import video from '../assets/vid1.mp4';
+
 export const Video = () => {
   return (
     <div>
             <h2>Video Section</h2>
             <video controls>
-                <source src={video} type="video/mp4" />
+                <source type="video/mp4" />
                
             </video>
             
