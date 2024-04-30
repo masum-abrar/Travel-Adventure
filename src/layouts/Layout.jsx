57df5465ff47f3395928944a7e18a6559409fd06
masum-abrar <masum.abrar02@gmail.com>
2024-04-30 20:37:28 +0600

    delete functionality modified

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
+
 
 
 
 
 export const Layout = () => {
   return (
-   <div data-aos="fade-in"     >
+   <div   >
 <Navbar></Navbar>
    <Outlet>
     <Home></Home>
