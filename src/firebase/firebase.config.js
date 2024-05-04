6f69db2d86c7b0493263f0099b2bda206f0a0d9b
masum-abrar <masum.abrar02@gmail.com>
2024-05-04 14:16:12 +0600

    functionality updated

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC09M-Vc-egLsCoJngBDKxCkrEE2CsnfPo",
-  authDomain: "travel-adventure-4404b.firebaseapp.com",
-  projectId: "travel-adventure-4404b",
-  storageBucket: "travel-adventure-4404b.appspot.com",
-  messagingSenderId: "1040800193018",
-  appId: "1:1040800193018:web:d31dbb351d6da6c69fac43"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
